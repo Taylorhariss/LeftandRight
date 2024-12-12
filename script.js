6b09660d872d03b8b2660857aecfe913e12ecbd9
Taylorhariss <tharris96@cps.edu>
2024-12-12 14:50:50 +0000

    x

--- script.js
+++ script.js
@@ -4,17 +4,38 @@ let leftRight = document.querySelector('.leftRight');
 let text = document.querySelector('p');
 
  // Choice A
-let basketball = document.querySelector('.basketball');
-basketball.style.display ="none";
 
+ let basketball = document.querySelector('.basketball');
+    basketball.style.display ="none";
 
+    let player = document.querySelector('.player');
+    player.style.display ="none";
+//left
 left.addEventListener('click', function() {
-
+    
+console.log("you choosed left");
     leftRight.style.display ="none";
     left.style.display ="none";
     right.style.display ="none";
     basketball.style.display ="block";
+    text.innerHTML ="to shoot click";
 
-    text.innerHTML ="to shoot double click"
+    
+});
+
+basketball.addEventListener('click', function() {
+
+console.log("you single clicked");
+leftRight.style.display ="none";
+left.style.display ="none";
+right.style.display ="none";
+player.style.display ="block";
+basketball.style.display ="none";
+text.innerHTML ="you made the shot";
+
 
 });
+
+
+
+
