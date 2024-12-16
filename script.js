63114739661f08e1b8544d84950a64ae962a6c01
Taylorhariss <tharris96@cps.edu>
2024-12-16 14:19:30 +0000

    weee

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let  left = document.querySelector('.left');
-let  right = document.querySelector('.right');
+let  kyrie = document.querySelector('.kyrie');
+let  bron = document.querySelector('.bron');
 let leftRight = document.querySelector('.leftRight');
 let text = document.querySelector('p');
 
@@ -10,13 +10,27 @@ let text = document.querySelector('p');
 
     let player = document.querySelector('.player');
     player.style.display ="none";
-//left
-left.addEventListener('click', function() {
+
+let defense = document.querySelector('.defense');
+    defense.style.display ="none";
+
+let block = document.querySelector('.block');
+    block.style.display ="none";
+
+let poster = document.querySelector('.poster');
+    poster.style.display ="none";
+
+let celebrate = document.querySelector('.celebrate');
+    celebrate.style.display ="none";
+
+
+//kyrie
+kyrie.addEventListener('click', function() {
     
-console.log("you choosed left");
+console.log("you choose kyrie");
     leftRight.style.display ="none";
-    left.style.display ="none";
-    right.style.display ="none";
+   kyrie.style.display ="none";
+   bron.style.display ="none";
     basketball.style.display ="block";
     text.innerHTML ="to shoot click";
 
@@ -26,16 +40,68 @@ console.log("you choosed left");
 basketball.addEventListener('click', function() {
 
 console.log("you single clicked");
-leftRight.style.display ="none";
-left.style.display ="none";
-right.style.display ="none";
+
+
 player.style.display ="block";
 basketball.style.display ="none";
-text.innerHTML ="you made the shot";
+text.innerHTML ="you made the shot, now click again";
 
 
 });
 
+
+player.addEventListener('click', function() {
+
+    console.log("you got back on defense");
+    leftRight.style.display ="none";
+   kyrie.style.display ="none";
+    bron.style.display ="none";
+    player.style.display ="none";
+    basketball.style.display ="none";
+    defense.style.display = "block";
+    text.innerHTML ="you got back on defense";
+
+});
+
+// Choice B
+bron.addEventListener('click', function() {
+    
+    console.log("you choosed right");
+        leftRight.style.display ="none";
+        kyrie.style.display ="none";
+        bron.style.display ="none";
+        block.style.display ="block";
+        text.innerHTML ="to block wheel";
+    
+        
+    });
+
+block.addEventListener('wheel', function() {
+
+        console.log("you postered opponent");
+        leftRight.style.display ="none";
+        kyrie.style.display ="none";
+       bron.style.display ="none";
+        block.style.display ="none";
+        poster.style.display ="block";
+        text.innerHTML ="to poster click";
+
+});
+
+poster.addEventListener('click', function() {
+        console.log("now you can celebrate");
+        leftRight.style.display ="none";
+        kyrie.style.display ="none";
+        bron.style.display ="none";
+        block.style.display ="none";
+        poster.style.display ="none";
+        celebrate.style.display ="block";
+        text.innerHTML ="now celebrate";
+
+
+
+});
+    
 
 
 
